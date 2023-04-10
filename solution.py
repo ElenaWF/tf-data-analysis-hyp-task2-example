3fee6c9d8fbb30c370cf7a3912c233003932e8d7
ElenaWF <129698720+ElenaWF@users.noreply.github.com>
2023-04-11 01:32:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
 
+from hyppo.ksample import MMD
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+
+chat_id = 1395253289 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.01
+    m = "laplacian"
+    g = 0.8
+    pval = MMD(compute_kernel=m, gamma=g).test(x, y).pvalue
+    return pval < alpha # Ваш ответ, True или False
